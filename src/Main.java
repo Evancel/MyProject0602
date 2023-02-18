2bc490a0c92de4dc2476369dbf1685d0f50e84c9
wlexa <w.lexandra@gmail..com>
2023-02-18 10:05:57 +0300

    The last commit, to save changes before upgrade windows

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,30 @@
 public class Main {
-
+    static Employee[] employee = new Employee[10];
     public static void main(String[] args) {
-        Employee[] employee = new Employee[10];
+
         employee[0] = new Employee("Иванов", "Иван", "Иванович", 2, 120000);
         employee[1] = new Employee("Петров", "Петр", "Петрович", 3, 70000);
         employee[2] = new Employee("Кузьмин", "Андрей", "Андреевич", 4, 65000);
         employee[3] = new Employee("Семенов", "Семен", "Семенович", 4, 90000);
 
-        printAllEmployees(employee);
-        System.out.println(sumSalary(employee));
-        System.out.println("Первый сотрудник с минимальной зарплатой: " + minSalary(employee));
-        System.out.println("Первый сотрудник с максимальной зарплатой: " + maxSalary(employee));
-        System.out.println(avgSalary(employee));
-        printAllFIO(employee);
+        printAllEmployees();
+        System.out.println(sumSalary());
+        System.out.println("Первый сотрудник с минимальной зарплатой: " + minSalary());
+        System.out.println("Первый сотрудник с максимальной зарплатой: " + maxSalary());
+        System.out.println(avgSalary());
+        printAllFIO();
 
-
+        increaseSalaryToAllEmployees(0.05);
+        printAllEmployees();
     }
 
-    public static void printAllEmployees(Employee[] employee) {
+    public static void printAllEmployees() {
         for(int i =0; i < employee.length && (i+1)<Employee.getCounterId(); i++) {
             System.out.println(employee[i]);
         }
     }
 
-    public static int sumSalary(Employee[] employee){
+    public static int sumSalary(){
         int sumSalary = 0;
 
         for (int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++) {
@@ -32,10 +33,11 @@ public class Main {
         return sumSalary;
     }
 
-    public static Employee minSalary (Employee[] employee){
+
+    public static Employee minSalary (){
         //находим минимальную зарплату
         int minSalary = employee[0].getSalary();
-        for(int i=0; i< employee.length && employee[i]!=null; i++){
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++){
             if(minSalary>employee[i].getSalary()){
                 minSalary = employee[i].getSalary();
             }
@@ -51,10 +53,10 @@ public class Main {
         return empWithMinSalary;
     }
 
-    public static Employee maxSalary (Employee[] employee){
+    public static Employee maxSalary (){
         //находим минимальную зарплату
         int maxSalary = employee[0].getSalary();
-        for(int i=0; i< employee.length && employee[i]!=null; i++){
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++){
             if(maxSalary<employee[i].getSalary()){
                 maxSalary = employee[i].getSalary();
             }
@@ -62,7 +64,7 @@ public class Main {
 
         Employee empWithMinSalary = employee[0];
 
-        for(int i=0; i< employee.length && employee[i]!=null; i++) {
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++) {
             if(employee[i].getSalary() == maxSalary){
                 empWithMinSalary = employee[i];
             }
@@ -70,14 +72,33 @@ public class Main {
         return empWithMinSalary;
     }
 
-    public static double avgSalary(Employee[] employee){
-        return sumSalary(employee) * 1.0 / (Employee.getCounterId()-1);
+    public static double avgSalary(){
+        return sumSalary() * 1.0 / (Employee.getCounterId()-1);
     }
 
-    public static void printAllFIO(Employee[] employee){
-        for(int i=0; i< employee.length && employee[i]!=null; i++) {
+    public static void printAllFIO(){
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++) {
             employee[i].printFIO();
         }
     }
+
+    public static void increaseSalaryToAllEmployees(double increaseRate){
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++){
+            employee[i].setSalary((int)(employee[i].getSalary()*(1+increaseRate)));
+        }
+    }
+
+    public static Employee searchEmployeeWithMinSalaryInDepartment (int department){
+        for(int i=0; i< employee.length && (i+1)<Employee.getCounterId(); i++){
+            if (employee[i].getDepartment()==department) {
+                int minSalaryInDepartment = employee[i].getSalary();
+                for (int i=0; )
+            }
+            }
+        }
+    }
+
+
+
 
 }
